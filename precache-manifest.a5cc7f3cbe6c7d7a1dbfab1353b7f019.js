2928e3a659208795c0d920e43822c7192cc8dc61
ozline <ozline@users.noreply.github.com>
2024-03-09 13:03:10 +0000

    deploy: f628e841d4fb57e5839944fa37a5f17c2a0ae905

---
+++ precache-manifest.a5cc7f3cbe6c7d7a1dbfab1353b7f019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c26a679e429bb095b9a0",
+    "url": "/static/css/main.a4e83720.chunk.css"
+  },
+  {
+    "revision": "c26a679e429bb095b9a0",
+    "url": "/static/js/main.c26a679e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "b3f25ad9f2be8ed22cb0",
+    "url": "/static/css/2.0874799b.chunk.css"
+  },
+  {
+    "revision": "b3f25ad9f2be8ed22cb0",
+    "url": "/static/js/2.b3f25ad9.chunk.js"
+  },
+  {
+    "revision": "e4feece783dd4eb83e21693cc4d058fd",
+    "url": "/index.html"
+  }
+];
